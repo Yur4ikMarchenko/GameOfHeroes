527974c1f0e4122be9fff236a5294d32ae4b1cf2
Yur4ikMarchenko <marchenko_ak19@nuwm.edu.ua>
2021-02-14 14:17:57 +0200

    Створення меню (без функціонала)

--- Source.cpp
+++ Source.cpp
@@ -8,8 +8,25 @@ using namespace std;
 
 int main()
 {
+	string comeback;
+	string path = "history.txt";
 	
+	fstream fs;
+
+	//меню
+menu:
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "~~~~~~~~~~~~ Game of Heroes ~~~~~~~~~~~~~~" << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "\t\tStart" << endl;
+	cout << "\t\tHistory" << endl;
+	cout << "\t\tAbout" << endl;
+	cout << "\t\tExit" << endl;
+
+	cout << "What do you want? : ";
+
+	string choose_menu;
+	cin >> choose_menu;
 
 	return 0;
-	
 }
